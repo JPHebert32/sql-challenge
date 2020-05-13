1ca3416a5899cdae509a3a7eee35e2c142f732f8
JP Hebert <JPHebert32@gmail.com>
2020-05-13 17:17:57 -0600

    added queries 1 & 2

---
+++ EmployeeSQL/employee_info.sql
@@ -0,0 +1,13 @@
+--List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+DROP VIEW employee_info;
+
+CREATE VIEW employee_info AS
+SELECT e.emp_no, e.last_name, e.first_name, e.sex
+FROM employees e 
+JOIN salaries s 
+ON e.emp_no = s.emp_no
+GROUP BY e.emp_no 
+ORDER BY e.emp_no ASC;
+
+SELECT * FROM employee_info;
