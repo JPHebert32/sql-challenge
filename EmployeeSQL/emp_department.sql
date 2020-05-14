9583760222e696b06cdecdba4e817103709afc67
JP Hebert <JPHebert32@gmail.com>
2020-05-14 02:10:22 -0600

    Completed 4

---
+++ EmployeeSQL/emp_department.sql
@@ -0,0 +1,11 @@
+DROP VIEW emp_department;
+
+CREATE VIEW emp_department AS
+SELECT e.emp_no, e.last_name, e.first_name,d.dept_name
+FROM ((employees e
+INNER JOIN dept_employees de
+	   ON e.emp_no = de.emp_no)
+INNER JOIN departments d 
+	  ON de.dept_no = d.dept_no);
+
+SELECT * FROM emp_department;
