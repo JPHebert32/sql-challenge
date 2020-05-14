8b33960ac9835af17fba039aab22e1e0a91ad35b
JP Hebert <JPHebert32@gmail.com>
2020-05-14 02:01:12 -0600

    Completed 3,5,8

--- EmployeeSQL/employee_info.sql
+++ EmployeeSQL/employee_info.sql
@@ -3,7 +3,7 @@
 DROP VIEW employee_info;
 
 CREATE VIEW employee_info AS
-SELECT e.emp_no, e.last_name, e.first_name, e.sex
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, Max(s.salary)AS "Salary"
 FROM employees e 
 JOIN salaries s 
 ON e.emp_no = s.emp_no
