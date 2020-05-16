8662f8a8b0173ce5d8cc9ab689ab19bbe6f604eb
JP Hebert <JPHebert32@gmail.com>
2020-05-16 15:29:46 -0600

    Average Salary Chart

--- EmployeeSQL/PH_Employees_DB.sql
+++ EmployeeSQL/PH_Employees_DB.sql
@@ -1,5 +1,13 @@
--- Link to schema: https://app.quickdatabasediagrams.com/#/d/LsFo3S
--- Physical
+-- DROP EXISTING TABLES
+DROP TABLE departments;
+DROP TABLE dept_employees;
+DROP TABLE dept_managers;
+DROP TABLE employees;
+DROP TABLE salaries;
+DROP TABLE titles;
+
+-- CREATE TABLES
+
 CREATE TABLE employees (
     emp_no INT   NOT NULL,
     emp_title_id VARCHAR(5)   NOT NULL,
@@ -75,3 +83,127 @@ SELECT * FROM dept_managers;
 SELECT * FROM employees;
 SELECT * FROM salaries;
 SELECT * FROM titles;
+
+----------------------------------------------------------------------------------------------
+-- 1. List the following details of each employee:
+----- employee number, last name, first name, sex, and salary.
+----------------------------------------------------------------------------------------------
+DROP VIEW employee_info;
+
+CREATE VIEW employee_info AS
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, Max(s.salary)AS "Salary"
+FROM employees e 
+JOIN salaries s 
+ON e.emp_no = s.emp_no
+GROUP BY e.emp_no 
+ORDER BY e.emp_no ASC;
+
+SELECT * FROM employee_info;
+
+----------------------------------------------------------------------------------------------
+-- 2. List first name, last name, and hire date for employees who were hired in 1986.
+----------------------------------------------------------------------------------------------
+DROP VIEW hired_86;
+
+CREATE VIEW hired_86 AS
+SELECT e.first_name, e.last_name, e.hire_date
+FROM employees e
+WHERE e.hire_date  BETWEEN '01/01/1986' AND '12/31/1986' 
+ORDER BY e.emp_no ASC;
+
+SELECT * FROM hired_86;
+
+------------------------------------------------------------------------------------------------
+-- 3. List the manager of each department with the following information:
+----- department number, department name, the manager's employee number, last name, first name.
+------------------------------------------------------------------------------------------------
+DROP VIEW managers;
+
+CREATE VIEW managers AS
+SELECT d.dept_no,d.dept_name,e.emp_no, e.last_name, e.first_name
+FROM ((employees e
+INNER JOIN dept_managers m
+	   ON e.emp_no = m.emp_no)
+INNER JOIN departments d 
+	  ON m.dept_no = d.dept_no);
+
+SELECT * FROM managers;
+
+------------------------------------------------------------------------------------------------
+-- 4. List the department of each employee with the following information:
+----- employee number, last name, first name, and department name.
+------------------------------------------------------------------------------------------------
+DROP VIEW emp_department;
+
+CREATE VIEW emp_department AS
+SELECT e.emp_no, e.last_name, e.first_name,d.dept_name
+FROM ((employees e
+INNER JOIN dept_employees de
+	   ON e.emp_no = de.emp_no)
+INNER JOIN departments d 
+	  ON de.dept_no = d.dept_no);
+
+SELECT * FROM emp_department;
+
+------------------------------------------------------------------------------------------------
+-- 5. List first name, last name, and sex for employees whose:
+----- first name is "Hercules" and last names begin with "B."
+------------------------------------------------------------------------------------------------
+DROP VIEW hercules;
+
+Create VIEW hercules AS
+SELECT e.first_name, e.last_name, e.sex
+FROM employees e
+WHERE e.last_name LIKE 'B%' AND e.first_name = 'Hercules';
+
+SELECT * FROM hercules;
+
+------------------------------------------------------------------------------------------------
+-- 6. List all employees in the Sales department:
+----- Including their employee number, last name, first name, and department name.
+------------------------------------------------------------------------------------------------
+DROP VIEW sales_dept;
+
+CREATE VIEW sales_dept AS
+SELECT e.emp_no, e.last_name, e.first_name,d.dept_name
+FROM ((employees e
+INNER JOIN dept_employees de
+	   ON e.emp_no = de.emp_no)
+INNER JOIN departments d 
+	  ON de.dept_no = d.dept_no);
+
+SELECT * FROM sales_dept WHERE dept_name = 'Sales';
+
+------------------------------------------------------------------------------------------------
+-- 7. List all employees in the Sales and Development departments:
+----- Including their employee number, last name, first name, and department name.
+------------------------------------------------------------------------------------------------
+DROP VIEW sales_dev;
+
+CREATE VIEW sales_dev AS
+SELECT e.emp_no, e.last_name, e.first_name,d.dept_name
+FROM ((employees e
+INNER JOIN dept_employees de
+	   ON e.emp_no = de.emp_no)
+INNER JOIN departments d 
+	  ON de.dept_no = d.dept_no)
+ORDER BY d.dept_name;
+
+SELECT * FROM sales_dev WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+------------------------------------------------------------------------------------------------
+-- 8. In descending order, list the frequency count of employee last names
+----- i.e., how many employees share each last name.
+------------------------------------------------------------------------------------------------
+DROP VIEW last_name;
+
+CREATE VIEW last_name AS
+SELECT e.last_name,
+    COUNT(*) occurrences
+FROM employees e
+GROUP BY e.last_name
+ORDER BY occurrences DESC;
+
+SELECT * FROM last_name;
+
+
